000619bfeb5c55d0750d0d6e24a6a9990f7a6b9b
toilabaodai123 <74287026+toilabaodai123@users.noreply.github.com>
2021-01-10 00:39:07 +0700

    load xuất chiếu lên vé
    load xuất chiếu lên vé

--- app/src/main/java/com/example/drawerfragment/Datve.java
+++ app/src/main/java/com/example/drawerfragment/Datve.java
@@ -2,13 +2,35 @@ package com.example.drawerfragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class Datve extends AppCompatActivity {
     private TextView tenphimm ,taikhoan;
+    ArrayList<String> xuatchieus;
+    ArrayList<String> xuatchieusx;
+    Spinner spinner;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,5 +44,44 @@ public class Datve extends AppCompatActivity {
         tenphimm.setText(tenphim);
         taikhoan=findViewById(R.id.testtaikhoannguoidung);
         taikhoan.setText(taikhoann);
+
+        loadxuatchieu();
+
+
+
+        //loadrap();
+        //loadghe();
     }
+    private void loadxuatchieu() {
+        String url = "http://192.168.1.103/apiloadxuatchieu.php";
+        xuatchieus=new ArrayList<>();
+        JsonArrayRequest jsonArrayRequest =new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for(int i = 0;i<response.length();i++){
+                    try {
+                        Toast.makeText(getApplicationContext(),response.toString(), Toast.LENGTH_SHORT).show();
+                        JSONObject object = response.getJSONObject(i);
+                        //String idxuatchieu = object.getString("id");
+                        String dmy = object.getString("dmy");
+                        xuatchieus.add(dmy);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                spinner=(Spinner)findViewById(R.id.snxuatchieu_datve);
+                ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_spinner_item,xuatchieus);
+                arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(arrayAdapter);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        Volley.newRequestQueue(this).add(jsonArrayRequest);
+    }
+
 }
---
+++ app/src/main/java/com/example/drawerfragment/XuatChieu.java
@@ -0,0 +1,18 @@
+package com.example.drawerfragment;
+
+public class XuatChieu {
+    public XuatChieu(String id, String dmy) {
+        this.id = id;
+        this.dmy = dmy;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDmy() {
+        return dmy;
+    }
+
+    String id ,dmy;
+}
